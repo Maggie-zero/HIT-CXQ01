5910f75abfd2fb63922ecb5a873706f74884c1bc
Maggie <bertzero28@gmail.com>
2024-12-04 11:15:02 +0800

    121204

--- src/router/views.ts
+++ src/router/views.ts
@@ -45,15 +45,15 @@ export default [
         },
         component: () => import('@/view/product/imgProcess/index.vue')
       },
-      {
-        path: '/home2',
-        meta: {
-          level: 1,
-          title: '监控',
-          icon: 'stopwatch'
-        },
-        component: () => import('@/view/sys_common/intro.vue')
-      },
+      // {
+      //   path: '/home2',
+      //   meta: {
+      //     level: 1,
+      //     title: '监控',
+      //     icon: 'stopwatch'
+      //   },
+      //   component: () => import('@/view/sys_common/intro.vue')
+      // },
     ]
   },
   // 科研模式示例
